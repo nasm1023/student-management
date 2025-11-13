282c91558f398b7bb12e7a80ad94880ea9e29f8d
namtl <namtl432@gmail.com>
2025-11-13 15:16:07 +0700

    fix: build BE

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import app from './src/app.js'
-// import {logger, requestLogger} from './src/utils/winston.js'
+import { logger, requestLogger } from './src/utils/winston.js'
 
-// app.use(requestLogger); // Middleware log request
-// const PORT = 3001
-// app.listen(PORT, () => logger.info({ message: `Server running on port ${PORT}` }));
+const PORT = 3001
+app.listen(PORT, () => logger.info({ message: `Server running on port ${PORT}` }));
 
 export default app;
